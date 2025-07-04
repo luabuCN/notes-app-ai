bc9622f7607f43693f84cb0a39ff9750c3b07325
张陈林 <2939804365@qq.com>
2025-07-04 16:36:42 +0800

    refactor(middleware): 更新中间件匹配路径以排除特定路由
    - 将中间件的匹配路径从'/editor'修改为更复杂的正则表达式，以排除'_next/static'、'_next/image'、'favicon.ico'、'signin'、'signup'等路由

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,5 +10,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/editor"],
+	matcher: [
+		"/((?!_next/static|_next/image|favicon.ico|signin|signup|$).*)",
+	],
 };
